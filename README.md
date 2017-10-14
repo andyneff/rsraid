490113168baa40fdcca2da77ffb65ca46ff225fe
Andrew Neff <andyneff@users.noreply.github.com>
2017-10-13 23:18:59 -0400

    Can decode now

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # rsraid
 
-Inelegant quick application to call utamaro/rsraid. Yeah... I don't know go
+Inelegant quick application to call utamaro/rsraid. Yay, my first (and LAST?!)
+go program
+
+## Build
+
+```
+go get github.com/andyneff/rsraid
+go build github.com/andyneff/rsraid
+```
+
+## Encode
+
+```
+ls / > test.txt
+rsraid -n 2 -m 1 -e test.txt
+```
+
+## Decode
+
+```
+rsraid -n 2 -d -s <size of original file> -o recombined.txt test_0.txt test_1.txt
+```
+
+## Bugs
+
+Well YEAH! The underlining library does it wrong. For example a 1299 size file
+split with `-n 2` breaks (it drops two bytes), but size 1300 works.
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
+import "strings"
+import "strconv"
 // import "fmt"
+import "os"
 import "flag"
 import "github.com/utamaro/rsraid"
 
@@ -9,11 +12,11 @@ func main(){
   nPtr := flag.Int("n", 2, "Number of files to split across (data devices)")
 
   // encodeNamePtr := flag.String("encode", "", "Encode (Split)")
-  encodePtr := flag.Bool("encode", false, "Encode (Split)")
-  decodePtr := flag.Bool("decode", false, "Decode (Combine)")
+  encodePtr := flag.Bool("e", false, "Encode (Split)")
 
-  outputPtr := flag.String("out", "", "Output filename")
-  output := *outputPtr
+  decodePtr := flag.Bool("d", false, "Decode (Combine)")
+  outputPtr := flag.String("o", "", "Output filename (for decoding)")
+  sizePtr := flag.Int("s", 0, "Size of output filename (for decoding)")
 
   flag.Parse()
   
@@ -25,16 +28,36 @@ func main(){
     panic("You must encode or decode")
   }
 
-  if *decodePtr && output == "" {
+  if *decodePtr && *outputPtr == "" {
     panic("You must specify an output file")
   }
 
   if *encodePtr{
     rsraid.EncodeFile(*mPtr, *nPtr, flag.Args()[0])
   } else {
-    panic("The encoding wasn't what I wanted. So I moved on")
-    //fnames := flag.Args()
-    //s:=int64(43545234) //size of test.dat
-    //rsraid.DecodeFile(s, *nPtr, fnames, *outputPtr)
+    fileNames := make([]string, *mPtr + *nPtr)
+    for _,i := range flag.Args() {
+      if i != "" {
+        file_parts := strings.Split(i, "_")
+        file_part := file_parts[len(file_parts)-1]
+        last_parts := strings.Split(file_part, ".")
+        num, _ := strconv.ParseInt(last_parts[0], 10, 64)
+        fileNames[num] = i
+      }
+    }
+
+    var size int64
+
+    // if the size isn't specified
+    if *sizePtr == 0 {
+      // GUESS! You have a 1 in n chance of being right! :D I thought about doing
+      // All n possibilities, but the utamaro/rsraid is too bugged to care
+      s, _ := os.Stat(flag.Args()[0])
+      size = int64(*nPtr) * s.Size()
+    } else {
+      size = int64(*sizePtr)
+    }
+
+    rsraid.DecodeFile(size, *nPtr, fileNames, *outputPtr)
   }
 }
